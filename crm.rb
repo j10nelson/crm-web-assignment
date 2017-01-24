c36ba4b21835d89e77f9cc5a3373306f857f9ae4
Justin <justintenser@me.com>
2017-01-24 16:38:06 -0500

    step1

--- crm.rb
+++ crm.rb
@@ -1,31 +1,8 @@
+# Implement the new web-based CRM here.
+# Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
+# You'll have to implement it from scratch.
 require 'sinatra'
-require_relative 'contact'
-
-# # Creating some fake data
-# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
-# #Done creating fake data
-# 3
 
 get '/' do
-
-  @crm_app_name = "Alex's CRM"
-
-  erb :index
-end
-
-get '/contacts' do
-  erb :contacts
-end
-
-post '/contacts' do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-
-  redirect to('/contacts')
-
-end
-
-get '/contacts/new' do
-  erb :new
+  'Main Menu'
 end
