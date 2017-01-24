707f4029973264ddc0b0c26d8d998426b7df8864
Alex Flint <me@alexf.ca>
2017-01-24 12:38:01 -0500

    created new.erb

--- crm.rb
+++ crm.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require_relative 'contact'
+
+# Creating some fake data
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+#Done creating fake data
+
 
 get '/' do
+
+  @crm_app_name = "Alex's CRM"
+
   erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :add
 end
